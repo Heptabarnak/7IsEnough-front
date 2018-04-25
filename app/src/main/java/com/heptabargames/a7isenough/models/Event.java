aee3e83f318414fee74cf935c2ae46eb8c5556da
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2018-04-25 10:51:38 +0200

    Delete map

--- app/src/main/java/com/heptabargames/a7isenough/models/Event.java
+++ app/src/main/java/com/heptabargames/a7isenough/models/Event.java
@@ -1,6 +1,7 @@
 package com.heptabargames.a7isenough.models;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 /**
  * Created by Julien on 25/04/2018.
@@ -13,15 +14,17 @@ public class Event {
     private String description;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
-    private Map map;
+    private ArrayList<Zone> zones;
+    private ArrayList<Sector> sectors;
 
-    public Event(int id, String name, String description, LocalDateTime startDate, LocalDateTime endDate, Map map) {
+    public Event(int id, String name, String description, LocalDateTime startDate, LocalDateTime endDate) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.map = map;
+        this.zones = new ArrayList<Zone>();
+        this.sectors = new ArrayList<Sector>();
     }
 
     public int getId() {
@@ -48,12 +51,20 @@ public class Event {
         this.description = description;
     }
 
-    public Map getMap() {
-        return map;
+    public ArrayList<Zone> getZones() {
+        return zones;
     }
 
-    public void setMap(Map map) {
-        this.map = map;
+    public void addZone(Zone zone) {
+        this.zones.add(zone);
+    }
+
+    public ArrayList<Sector> getSectors() {
+        return sectors;
+    }
+
+    public void addSectors(Sector sector) {
+        this.sectors.add(sector);
     }
 
     public LocalDateTime getStartDate() {
