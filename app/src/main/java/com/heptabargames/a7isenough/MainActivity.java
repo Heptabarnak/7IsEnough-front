e84dd23cb131ead84243faf68c44751aa89046e0
Justin GALLY <justin.gallysp@gmail.com>
2018-04-26 19:15:25 +0200

    Adding of lateral menu

--- app/src/main/java/com/heptabargames/a7isenough/MainActivity.java
+++ app/src/main/java/com/heptabargames/a7isenough/MainActivity.java
@@ -1,15 +1,24 @@
 package com.heptabargames.a7isenough;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -32,16 +41,43 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private FloatingActionButton.OnClickListener fabQRListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "QR Clicked", Toast.LENGTH_SHORT).show();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.app_topbar);
-        setSupportActionBar(toolbar);
+        Toolbar lateralbar = findViewById(R.id.lateralbar_toolbar);
+        setSupportActionBar(lateralbar);
+        lateralbar.setTitleTextColor(Color.GRAY);
+
+        drawer = findViewById(R.id.app_lateralbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, lateralbar, R.string.lateral_menu_open, R.string.lateral_menu_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.app_bottombar);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FloatingActionButton button = (FloatingActionButton) findViewById(R.id.qr_button);
+        button.setOnClickListener(fabQRListener);
     }
 
+    ;
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
