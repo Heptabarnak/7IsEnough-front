35bac1f7bd3f10885b2d23320285b3fdf3aa8ed1
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2018-04-27 11:44:18 +0200

    Callback example

--- app/src/main/java/com/heptabargames/a7isenough/listeners/LocationListener.java
+++ app/src/main/java/com/heptabargames/a7isenough/listeners/LocationListener.java
@@ -4,7 +4,9 @@ import android.location.Location;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.model.LatLng;
+import com.heptabargames.a7isenough.models.Event;
 import com.heptabargames.a7isenough.models.Zone;
+import com.heptabargames.a7isenough.services.EventService;
 import com.heptabargames.a7isenough.services.LocalizationService;
 
 import java.util.List;
@@ -13,10 +15,15 @@ import java.util.List;
  * Created by Julien on 27/04/2018.
  */
 
-public class LocationListener implements android.location.LocationListener{
+public class LocationListener implements android.location.LocationListener, OnManifestLoaded {
     @Override
     public void onLocationChanged(Location location) {
         LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+        EventService eventService = new EventService();
+
+
+        eventService.getManifest(this);
 
     }
 
@@ -32,6 +39,16 @@ public class LocationListener implements android.location.LocationListener{
 
     @Override
     public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onManifest(List<Event> events) {
+        
+    }
+
+    @Override
+    public void onError(Exception e) {
 
     }
 }
