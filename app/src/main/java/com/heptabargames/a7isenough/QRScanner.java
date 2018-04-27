166f506a15b059133c4ad26c936622995d96d490
Unknown <vincent.guillon@hotmail.fr>
2018-04-27 12:51:37 +0200

    Added Camera perssion request

--- app/src/main/java/com/heptabargames/a7isenough/QRScanner.java
+++ app/src/main/java/com/heptabargames/a7isenough/QRScanner.java
@@ -1,7 +1,11 @@
 package com.heptabargames.a7isenough;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -18,7 +22,14 @@ public class QRScanner extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_qrscanner);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED)
+        {
+            ActivityCompat.requestPermissions(QRScanner.this, new String[] {Manifest.permission.CAMERA}, 1);
+        }
+
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
         mCodeScanner.setDecodeCallback(new DecodeCallback() {
