597c43cda567b7569fdcb94bfa4669bcbe562fbe
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2018-04-27 09:09:35 +0200

    Polygon contains point

--- app/src/main/java/com/heptabargames/a7isenough/services/LocalizationService.java
+++ app/src/main/java/com/heptabargames/a7isenough/services/LocalizationService.java
@@ -1,5 +1,6 @@
 package com.heptabargames.a7isenough.services;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.heptabargames.a7isenough.models.Zone;
 
 import java.util.List;
@@ -7,9 +8,45 @@ import java.util.List;
 public class LocalizationService {
 
 
-    public Zone isInZone(Point point, List<Zone> zones) {
-
-
+    public Zone isInZone(LatLng point, List<Zone> zones) {
+        for(Zone z : zones){
+            if(isPointInPolygon(point, z.getPolygon())){
+                return z;
+            }
+        }
         return null;
+    }
+
+    private boolean isPointInPolygon(LatLng tap, List<LatLng> vertices) {
+        int intersectCount = 0;
+        for (int j = 0; j < vertices.size() - 1; j++) {
+            if (rayCastIntersect(tap, vertices.get(j), vertices.get(j + 1))) {
+                intersectCount++;
+            }
+        }
+
+        return ((intersectCount % 2) == 1); // odd = inside, even = outside;
+    }
+
+    private boolean rayCastIntersect(LatLng tap, LatLng vertA, LatLng vertB) {
+
+        double aY = vertA.latitude;
+        double bY = vertB.latitude;
+        double aX = vertA.longitude;
+        double bX = vertB.longitude;
+        double pY = tap.latitude;
+        double pX = tap.longitude;
+
+        if ((aY > pY && bY > pY) || (aY < pY && bY < pY)
+                || (aX < pX && bX < pX)) {
+            return false; // a and b can't both be above or below pt.y, and a or
+            // b must be east of pt.x
+        }
+
+        double m = (aY - bY) / (aX - bX); // Rise over run
+        double bee = (-aX) * m + aY; // y = mx + b
+        double x = (pY - bee) / m; // algebra is neat!
+
+        return x > pX;
     }
 }
