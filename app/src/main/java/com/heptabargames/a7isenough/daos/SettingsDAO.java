b4b5e7bc9f74c53efd0a38e35c7ff3751b5c8e7e
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2018-04-30 10:04:40 +0200

    Constructor

--- app/src/main/java/com/heptabargames/a7isenough/daos/SettingsDAO.java
+++ app/src/main/java/com/heptabargames/a7isenough/daos/SettingsDAO.java
@@ -1,5 +1,9 @@
 package com.heptabargames.a7isenough.daos;
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 import com.heptabargames.a7isenough.models.Beacon;
 
 import java.time.LocalDateTime;
@@ -7,6 +11,12 @@ import java.time.LocalDateTime;
 public class SettingsDAO {
 
     public static final String TAG = "SettingsDAO";
+
+    private RequestQueue requestQueue;
+
+    public SettingsDAO(Context context){
+        requestQueue = Volley.newRequestQueue(context);
+    }
 
     public void saveBeacon(Beacon beacon) {
 
