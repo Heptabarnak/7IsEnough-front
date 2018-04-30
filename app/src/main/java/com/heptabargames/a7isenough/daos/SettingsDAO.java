aaa4bd6813af3886ec4b0b2999e948571e5a23b1
Marc-Antoine Fernandes <embraser01@gmail.com>
2018-04-30 18:34:51 +0200

    Complete Settings DAO

--- app/src/main/java/com/heptabargames/a7isenough/daos/SettingsDAO.java
+++ app/src/main/java/com/heptabargames/a7isenough/daos/SettingsDAO.java
@@ -2,32 +2,83 @@ package com.heptabargames.a7isenough.daos;
 
 import android.content.Context;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.util.IOUtils;
 import com.heptabargames.a7isenough.models.Beacon;
+import com.heptabargames.a7isenough.models.Event;
 
-import java.time.LocalDateTime;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 public class SettingsDAO {
 
     public static final String TAG = "SettingsDAO";
 
-    private RequestQueue requestQueue;
+    private static final String GAME_FILE = "game_file.json";
+    private Context context;
 
-    public SettingsDAO(Context context){
-        requestQueue = Volley.newRequestQueue(context);
+    public SettingsDAO(Context context) {
+        this.context = context;
     }
 
-    public void saveBeacon(Beacon beacon) {
+    public void saveBeacon(Beacon beacon, Event event) throws IOException, JSONException {
+        JSONObject save = getJSON();
 
+        if (searchBeacon(beacon, event) == null) {
+            JSONArray beacons = save.optJSONArray(event.getId());
+
+            if (beacons == null) {
+                beacons = new JSONArray();
+                save.put(event.getId(), beacons);
+            }
+            JSONObject object = new JSONObject();
+
+            object.put("id", beacon.getId());
+            object.put("date", beacon.getFound().getTime());
+
+            beacons.put(object);
+
+            saveJSON(save);
+        }
     }
 
-    public LocalDateTime searchBeacon(Beacon beacon) {
+    private JSONObject getJSON() throws JSONException, IOException {
+        try (FileInputStream inputStream = context.openFileInput(GAME_FILE)) {
+
+            String result = new String(IOUtils.toByteArray(inputStream), "UTF-8");
+            return new JSONObject(result);
+        }
+    }
+
+    private void saveJSON(JSONObject object) throws IOException, JSONException {
+        try (FileOutputStream outputStream = context.openFileOutput(GAME_FILE, Context.MODE_PRIVATE)) {
+
+            outputStream.write(object.toString(4).getBytes());
+        }
+    }
+
+    public Date searchBeacon(Beacon beacon, Event event) throws IOException, JSONException {
+        JSONObject save = getJSON();
+
+        JSONArray beacons = save.optJSONArray(event.getId());
+
+        if (beacons != null) {
+            for (int i = 0; i < beacons.length(); i++) {
+                if (beacons.getJSONObject(i).getInt("id") == beacon.getId()) {
+                    return new Date(beacons.getJSONObject(i).getLong("date"));
+                }
+            }
+        }
 
         return null;
     }
 
     public void clear() {
-
+        context.deleteFile(GAME_FILE);
     }
 }
--- app/src/main/java/com/heptabargames/a7isenough/services/BeaconService.java
+++ app/src/main/java/com/heptabargames/a7isenough/services/BeaconService.java
@@ -1,11 +1,49 @@
 package com.heptabargames.a7isenough.services;
 
+import android.content.Context;
+
+import com.heptabargames.a7isenough.daos.SettingsDAO;
+import com.heptabargames.a7isenough.models.Beacon;
+import com.heptabargames.a7isenough.models.Event;
+import com.heptabargames.a7isenough.models.Zone;
+
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.util.Date;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 
 public class BeaconService {
 
-    public Date checkBeacon(String token) {
+    private SettingsDAO settingsDAO;
 
+    public BeaconService(Context context) {
+        this.settingsDAO = new SettingsDAO(context);
+    }
+
+    public Beacon checkBeacon(Event event, String token) throws IOException, JSONException {
+        // Create instance
+        Argon2 argon2 = Argon2Factory.create();
+
+        Beacon found = null;
+
+        for (Zone zone : event.getZones()) {
+            for (Beacon beacon : zone.getBeacons()) {
+                if (argon2.verify(beacon.getHash(), token)) {
+                    found = beacon;
+                    break;
+                }
+            }
+            if (found != null) break;
+        }
+
+        if (found != null) {
+            found.setFound(new Date());
+            settingsDAO.saveBeacon(found, event);
+            return found;
+        }
         return null;
     }
 }
